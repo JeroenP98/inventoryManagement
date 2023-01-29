66063bedc7232a35dca89be3eebbf3ca60eff53a
JeroenP98 <104794195+JeroenP98@users.noreply.github.com>
2023-01-29 16:54:20 +0100

    Update db_connect.php

--- php/include/db_connect.php
+++ php/include/db_connect.php
@@ -2,7 +2,7 @@
 
 
 $servername = "localhost";
-$username = "";
+$username = "root";
 $password = "";
 $database = "greenhome";
 $connection = new mysqli($servername, $username, $password, $database);
