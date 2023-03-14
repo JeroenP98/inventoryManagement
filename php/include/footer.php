733bd159359da6d7d46a3c82f88a47783faf5f45
Jeroen <jeroenpost@upcmail.nl>
2023-03-14 14:24:49 +0100

    see changelog 1.18

--- php/include/footer.php
+++ php/include/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3 border-top border-opacity-10">
     <div class="container d-flex justify-content-center">
       <span class="text-muted">&copy; <?=date("Y")?> GreenHome</span>
-      <span class="text-muted">/Version 1.15</span>
+      <span class="text-muted">/Version 1.18</span>
     </div>
   </footer>
