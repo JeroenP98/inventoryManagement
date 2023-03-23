fafb945b0f63e2c6f450f472e9d8a1180a0b2474
Jan van der Waal | Holland Ship Electric B.V <j.vanderwaal@hollandshipelectric.com>
2023-03-23 19:05:18 +0100

    headers toevoegen aan orders en orderline's

--- php/outgoing_orders/controller_orderCreator.php
+++ php/outgoing_orders/controller_orderCreator.php
@@ -95,6 +95,66 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>New Order | GreenHome</title>
   </head>
   <body>
+  <header class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="../../dashboard.php" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none me-5">
+          <img src="../../images/logo.png" alt="company logo" srcset="" width="40" height="40">
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-cßenter mb-md-0 nav-pills">
+          <li class="nav-item"><a href="../../dashboard.php" class="nav-link" aria-current="page">Dashboard</a></li>
+          <li class="nav-item"><a href="../articles/GUI_articles.php" class="nav-link">Articles</a></li>
+          <li class="nav-item"><a href="../stock/GUI_stock.php" class="nav-link">inventory</a></li>
+          <li class="nav-item"><a href="../relations/GUI_relations.php" class="nav-link" aria-current="page" >Relations</a></li>
+          <li class="nav-item"><a href="../incoming_orders/GUI_incoming.php" class="nav-link">Incoming orders</a></li>
+          <li class="nav-item"><a href="../outgoing_orders/GUI_outgoing.php" class="nav-link active">Outgoing orders</a></li>
+          <li class="nav-item"><a href="../users/GUI_users.php" class="nav-link">Users</a></li>
+          <li class="nav-item"><a  href="../companies/GUI_companies.php" class="nav-link">Companies</a></li>
+          <li class="nav-item "><a  href="../accessibilities/GUI_accessibilities.php" class="nav-link">Accesibility</a></li>
+          <li class="nav-item "><a  href="../functions/GUI_functions.php" class="nav-link">Functions</a></li>
+        </ul>
+        <?php
+          //either display the users first name when logged in or give the option to log themselves in
+          if(isset($_SESSION['user_id'])):?>
+            <div class='dropdown text-end'>
+            <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <?=$_SESSION['user_name']?>
+          </button>
+            <ul class='dropdown-menu text-small'>
+              <li><a class='dropdown-item' href='#' data-bs-toggle='modal' data-bs-target='#logOutModal'>Sign out</a></li>
+              <div class="form-check form-switch ms-3">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onclick="toggleTheme()">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Color theme</label>
+              </div>
+            </ul>
+          </div>
+          <?php else :?>
+            <div class='nav-item ml-auto'>
+            <a href='php/users/GUI_login.php' class='btn btn-outline-primary'>Login</a>
+            </div>
+          <?php endif;?>
+      </div>
+    </div>
+  </header>
+    <!-- start logout Modal -->
+    <div class="modal fade" id="logOutModal" tabindex="-1"     aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">You are about to log out!</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Are you sure?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Keep me logged in</button>
+            <a href="../users/GUI_logged_out.php"><button type="button" class="btn btn-warning">Log out</button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end logout modal-->
     <div class="container my-5">
       <h2>New Order</h2>
 
@@ -177,13 +237,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
           </div>
         </div>
       </form>
-    </div>
-    <hr class="row mb-3"/>
-
-    <div class="container my-5">
-     <h2>Orderline</h2
-     class="offset-sm-1 col-sm-2">
-     <a href"" class="btn btn-primary">Add Orderline</a>
     </div>
   </body>
   <?php 
