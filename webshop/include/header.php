d32483171fbf553fa8d2fbc85c85461885dabae2
Jeroen <jeroenpost@upcmail.nl>
2023-05-18 12:20:29 +0200

    added order functionality from webshop

--- webshop/include/header.php
+++ webshop/include/header.php
@@ -19,6 +19,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles.css">
     <link rel="shortcut icon" href="../../images/logo.png">
+    <!-- Links for Mapbox API tests
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet">
+    <script defer src="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js"></script>
+    <script defer src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
+    <script defer src="../js/mapbox_API.js"></script>
+    -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     
 
 </head>
--- webshop/shop/GUI_cart.php
+++ webshop/shop/GUI_cart.php
@@ -1,217 +1,204 @@
 <?php
 session_start();
-
 $page_name = "Cart | GreenHome";
-
 require_once "../include/header.php";
-
-echo print_r($_SESSION['cart']);
 ?>
 
-    <div class="container mt-0">
-    <main>
-      <div class="row g-5">
-        <div class="col-md-5 col-lg-4 order-md-last">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-primary">Your cart</span>
-            <span class="badge bg-primary rounded-pill">3</span>
-          </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">−$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
-
-          <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code">
-              <button type="submit" class="btn btn-secondary">Redeem</button>
-            </div>
-          </form>
+<main>
+  <div class="container justify-content-center">
+    <div class="row">
+      <h1 class="fw-bold">Winkelwagen & verzend details</h1>
+      <hr>
+      <?php if(isset($_GET['error_message'])):?>
+        <div class="alert alert-danger alert-dismissible fade show my-3" role="alert">
+          <p><strong>Oeps!</strong> <?=$_GET['error_message']?></p>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        <div class="col-md-7 col-lg-8">
-          <h4 class="mb-3">Billing address</h4>
-          <form class="needs-validation" novalidate>
-            <div class="row g-3">
-              <div class="col-sm-6">
-                <label for="firstName" class="form-label">First name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-                <div class="invalid-feedback">
-                  Valid first name is required.
-                </div>
-              </div>
-
-              <div class="col-sm-6">
-                <label for="lastName" class="form-label">Last name</label>
-                <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-                <div class="invalid-feedback">
-                  Valid last name is required.
-                </div>
-              </div>
-
-              <div class="col-12">
-                <label for="username" class="form-label">Username</label>
-                <div class="input-group has-validation">
-                  <span class="input-group-text">@</span>
-                  <input type="text" class="form-control" id="username" placeholder="Username" required>
-                <div class="invalid-feedback">
-                    Your username is required.
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-12">
-                <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                <input type="email" class="form-control" id="email" placeholder="you@example.com">
-                <div class="invalid-feedback">
-                  Please enter a valid email address for shipping updates.
-                </div>
-              </div>
-
-              <div class="col-12">
-                <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
-                <div class="invalid-feedback">
-                  Please enter your shipping address.
-                </div>
-              </div>
-
-              <div class="col-12">
-                <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-              </div>
-
-              <div class="col-md-5">
-                <label for="country" class="form-label">Country</label>
-                <select class="form-select" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please select a valid country.
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <label for="state" class="form-label">State</label>
-                <select class="form-select" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <label for="zip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder="" required>
-                <div class="invalid-feedback">
-                  Zip code required.
-                </div>
-              </div>
+      <?php endif; ?>
+    </div>
+    <div class="row">
+      <div class="col-md-5 col-lg-4 order-md-last">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+          <span class="text-primary">Winkelwagen</span>
+          <span class="badge bg-success rounded-pill">
+            <?php 
+            // calculate and show how many unique items are in the cart
+            $cart_count = !empty($_SESSION['cart']) ? count($_SESSION['cart']) : "0"; echo $cart_count;
+            ?>
+          </span>
+        </h4>
+        <ul class="list-group mb-3">
+          <?php 
+          // load each cart item into the cart overview
+          if(!empty($_SESSION['cart'])){
+            $cart = $_SESSION['cart'];
+            foreach($cart as $key => $cart_item){
+          ?>
+          <li class="list-group-item d-flex justify-content-between align-items-center lh-sm" id="<?=$key?>">
+            <div>
+              <h6 class="my-0"><?=$cart_item['article_name']?></h6>
             </div>
+            <div class="d-flex">
+              <form action="update_cart.php" method="POST">
+                <input type="hidden" name="action" value="decrease">
+                <input type="hidden" name="key" value="<?=$key?>">
+                <button class="badge btn bg-secondary rounded-pill decrease-quantity" type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                  </svg>
+                </button>
+              </form>
+              <span class="badge bg-primary rounded-pill quantity mx-1" data-cart-item-id="<?=$key?>"><?=$cart_item['quantity']?>x</span>
+              <form action="update_cart.php" method="POST">
+                <input type="hidden" name="action" value="increase">
+                <input type="hidden" name="key" value="<?=$key?>">
+                <button class="badge btn bg-secondary rounded-pill decrease-quantity" type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                  </svg>
+                </button>
+              </form>
 
-            <hr class="my-4">
-
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="same-address">
-              <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
             </div>
+            <form action="update_cart.php" method="POST">
+              <input type="hidden" name="action" value="delete">
+              <input type="hidden" name="key" value="<?=$key?>">
+              <button class="btn px-2 align-items-center delete-item" type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                </svg>
+              </button>
 
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="save-info">
-              <label class="form-check-label" for="save-info">Save this information for next time</label>
+            </form>
+          </li>
+          <?php
+          }} else {?>
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Je winkelwagen is leeg!</h6>
             </div>
+          </li>
 
-            <hr class="my-4">
+          <?php };  //close the if and foreach loop?>
 
-            <h4 class="mb-3">Payment</h4>
+        </ul>
 
-            <div class="my-3">
-              <div class="form-check">
-                <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
-                <label class="form-check-label" for="credit">Credit card</label>
-              </div>
-              <div class="form-check">
-                <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
-                <label class="form-check-label" for="debit">Debit card</label>
-              </div>
-              <div class="form-check">
-                <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
-                <label class="form-check-label" for="paypal">PayPal</label>
-              </div>
-            </div>
+        <hr class="my-4">
+        
+        <?php if(!empty($_SESSION['cart'])):?>
+        <ul class="list-group">
+          <li class="list-group-item fw-bold">
+          <h4 class="d-flex justify-content-between" id="cart-total">
+            <span class="text-primary">Totaal:</span>
+            €<?php 
+            $cart_total = 0;
+            foreach($cart as $cart_item){
+              $cart_total += ($cart_item['selling_price'] * $cart_item['quantity']);};
 
-            <div class="row gy-3">
-              <div class="col-md-6">
-                <label for="cc-name" class="form-label">Name on card</label>
-                <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                <small class="text-muted">Full name as displayed on card</small>
-                <div class="invalid-feedback">
-                  Name on card is required
-                </div>
-              </div>
-
-              <div class="col-md-6">
-                <label for="cc-number" class="form-label">Credit card number</label>
-                <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                <div class="invalid-feedback">
-                  Credit card number is required
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <label for="cc-expiration" class="form-label">Expiration</label>
-                <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                <div class="invalid-feedback">
-                  Expiration date required
-                </div>
-              </div>
-
-              <div class="col-md-3">
-                <label for="cc-cvv" class="form-label">CVV</label>
-                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                <div class="invalid-feedback">
-                  Security code required
-                </div>
-              </div>
-            </div>
-
-            <hr class="my-4">
-
-            <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-          </form>
-        </div>
+            echo $cart_total ?>
+          </h4>  
+          </li>
+        </ul>
+        <?php endif; ?>
       </div>
-    </main>
-    <?php require_once "../include/footer.php"?>
+      <div class="col-md-7 col-lg-8">
+        <h4 class="mb-3">Verzend details</h4>
+        <form class="needs-validation" method="POST" action="controller_checkout.php">
+          <?php 
+          
+          // disable form input fields if the cart is empty
+          if(empty($_SESSION['cart'])):?>
+          <fieldset disabled>
+          <?php endif; ?>
+          <div class="row g-3">
+
+            <div class="col-sm-6">
+              <label for="first_name" class="form-label">Voornaam</label>
+              <input type="text" class="form-control" id="first_name"  name="first_name" required>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="last_name" class="form-label">Achternaam</label>
+              <input type="text" class="form-control" id="last_name" name="last_name" required>
+            </div>
+
+            <div class="col-7">
+              <label for="email_adress" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email_adress" name="email_adress" required>
+            </div>
+
+            <div class="col-5">
+              <label for="phone_number" class="form-label">Telefoon</label>
+              <input type="text" class="form-control" id="phone_number" name="phone_number" required>
+            </div>
+
+            <div class="col-8">
+              <label for="street" class="form-label">Straat</label>
+              <input type="text" class="form-control" id="street" name="street" required>
+            </div>
+
+            <div class="col-4">
+              <label for="house_nr" class="form-label">Huisnummer</label>
+              <input type="text" class="form-control" id="house_nr" name="house_nr" required>
+            </div>
+
+            <div class="col-md-5">
+              <label for="country_code" class="form-label">Land</label>
+              <select class="form-control" id="country_code" name="country_code" required>
+                <option value="" disabled selected>Select a country</option>
+              </select>
+            </div>
+
+            <script>
+              // Make an HTTP GET request to the REST Countries API
+              fetch('https://restcountries.com/v3.1/all')
+                .then(response => response.json())
+                .then(data => {
+
+                  // Sort the data array by name.common
+                  data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+                  // Get a reference to the select field
+                  const select = document.getElementById('country_code');
+
+                  // Loop through the data and add each country code as an option
+                  data.forEach(country => {
+                    const option = document.createElement('option');
+                    option.value = country.cca2; // Use the cca2 property instead
+                    option.text = country.name.common; // Use the cca2 property instead
+                    select.add(option);
+                  });
+                })
+                .catch(error => console.error(error));
+            </script>
+
+
+
+            <div class="col-md-4">
+              <label for="city" class="form-label">Stad</label>
+              <input type="text" class="form-control" id="city"  name="city" required>
+            </div>
+
+            <div class="col-md-3">
+              <label for="zip_code" class="form-label">Postcode</label>
+              <input type="text" class="form-control" id="zip_code" name="zip_code" required>
+            </div>
+          </div>
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Place order</button>
+          <?php 
+          // close the fieldset tag when the cart is empty
+          if(empty($_SESSION['cart'])):?>
+          </fieldset>
+          <?php endif; ?>
+
+
+      
+        </form>
+      </div>
+    </div>
+
+  </div>
+</main>
+<?php require_once "../include/footer.php";?>
